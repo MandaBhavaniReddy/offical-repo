ebeba0c1191a5895d9b583812f22ec664d2e52a9
Bhavani <bhavani64732@gmail.com>
2024-10-21 16:02:30 +0530

    branch and merging commit

--- hello world.java
+++ hello world.java
@@ -1,7 +1,9 @@
-public class HelloWorld
+public class HelloWorld2
 {
-    public static void main(String[] args)
+    public static void main1(String[] args)
     {
         System.out.println("Hello My World")
+        System.out.println("second question in case study")
+        System.out.println(". Branching and Merging")
     }
 }
